774015d55bb4e813f1e25026edcce300d75c4c22
Maxirv22 <113070644+Maxirv22@users.noreply.github.com>
2022-09-30 23:23:38 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+/*let nombre = "Programación" + " Fácil.";
+let saludo = "Les doy la bienvenida a una nueva Lengua ";
+let fraseCompleta = saludo + nombre;
+console.log("Les doy la bienvenida a una nueva Lengua");
+
+let ven = "Divercion" + " Tiempo.";
+let juntos = "Les doy la bienvenida a una  nueva Lengua "
+let podemos = ven + juntos;
+console.log("Disfruta y diviertete");
+
+let nombreS1 = "Maxi";
+let nombreS2 = "Vero";
+let nombreS3 = "Angelo";
+let concatenaNombres = nombreS1 + nombreS2 + nombreS3;
+console.log(concatenaNombres);
+
+let nombre4;
+nombre4 = prompt("Tu Nombre");
+ alert("Hola_" + nombre4);
+let edad;
+edad = prompt("Ingresa tu Edad");
+if (edad >= 18) {
+    alert("Puedes entrar, Eres Mayor Disfruta y Aprende.");
+} else {
+    alert("Entra junto a un mayor, Diviertete y Aprende");
+} */
+
+function otraPagina(){
+    var confirmar = confirm('Estas Seguro deseas Salir');
+    if (confirmar){
+        window.location.href = 'https://www.google.com.'
+            }
+            else{
+                
+            }
+}
+
